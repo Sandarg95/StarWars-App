d2d0e32ab9daf7d3f872ae6e3b7d61c269092661
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-11-03 20:22:37 -0500

    Update SpaceScreen.js

--- screen/SpaceScreen.js
+++ screen/SpaceScreen.js
@@ -5,12 +5,12 @@ import styles from "../styles";
 
 export default function Space({ navigation }) {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text style={styles.title}>Welcome to the Space Page</Text>
-      <View style={styles.box}>
-        <Text style={styles.boxText}>DISCOVER THE STARWARS SPACE HERE!</Text>
-      </View>
-    </View>
-  );
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>Welcome to the Space Page</Text>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>Discover the Star Wars Space here!</Text>
+      </View>
+    </View>
+  );
 }
