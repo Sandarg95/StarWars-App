a67ca7fb22dfdabba33a178da5b6ae0ffbd9555c
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-11-03 20:22:18 -0500

    Update PlanetsScreen.js

--- screen/PlanetsScreen.js
+++ screen/PlanetsScreen.js
@@ -5,12 +5,12 @@ import styles from "../styles";
 
 export default function Planets({ navigation }) {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text style={styles.title}>Welcome to the Planets Page</Text>
-      <View style={styles.box}>
-        <Text style={styles.boxText}>DISCOVER THE STARWARS PLANETS HERE!</Text>
-      </View>
-    </View>
-  );
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>Welcome to the Planets Page</Text>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>Discover the Star Wars Planets here!</Text>
+      </View>
+    </View>
+  );
 }
