74848f186213fd7cf65f4cb2f27d877105210e5d
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-11-03 20:21:52 -0500

    Update FilmsScreen.js

--- screen/FilmsScreen.js
+++ screen/FilmsScreen.js
@@ -5,12 +5,12 @@ import styles from "../styles";
 
 export default function Films({ navigation }) {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text style={styles.title}>Welcome to the FilmS Page</Text>
-      <View style={styles.box}>
-        <Text style={styles.boxText}>DISCOVER THE STARWARS FILM HERE!</Text>
-      </View>
-    </View>
-  );
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>Welcome to the Films Page</Text>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>Discover the Star Wars Films here!</Text>
+      </View>
+    </View>
+  );
 }
