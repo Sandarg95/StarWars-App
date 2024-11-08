a814e40d86fdd393e2bca468c484c2c69f4d8278
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-11-08 02:44:26 -0500

    Update SpaceScreen.js

--- screen/SpaceScreen.js
+++ screen/SpaceScreen.js
@@ -1,16 +1,24 @@
-import React from "react";
-import { View, Text, Button, StatusBar } from "react-native";
-import styles from "../styles";
+import React from 'react';
+import { View, Text, StatusBar, SafeAreaView } from "react-native";
+import styles from "../components/styles";
+import ListContainer from "../components/ListContainer";
+import { fetchItemsSpace } from "../api"; // Import the fetch function
 
+function ListContainerSpace() {
+  return <ListContainer fetchFunction={fetchItemsSpace} />;
+}
 
 export default function Space({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden={false} barStyle="dark-content" />
       <Text style={styles.title}>Welcome to the Space Page</Text>
       <View style={styles.box}>
-        <Text style={styles.boxText}>Discover the Star Wars Space here!</Text>
+        <Text style={styles.boxText}>DISCOVER THE STARWARS SPACESHIP HERE!</Text>
       </View>
-    </View>
+      <View style={styles.container}>
+        <ListContainerSpace />
+      </View>
+    </SafeAreaView>
   );
 }
