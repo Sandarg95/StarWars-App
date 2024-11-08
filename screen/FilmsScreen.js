51d2407a9d7e807be86e48f22b4dd784b604ee18
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-11-08 02:43:54 -0500

    Update FilmsScreen.js

--- screen/FilmsScreen.js
+++ screen/FilmsScreen.js
@@ -1,16 +1,24 @@
-import React from "react";
-import { View, Text, Button, StatusBar } from "react-native";
-import styles from "../styles";
+import React from 'react';
+import { View, Text, StatusBar, SafeAreaView } from "react-native";
+import styles from "../components/styles";
+import ListContainer from "../components/ListContainer";
+import { fetchItemsFilms } from "../api"; 
 
+function ListContainerFilms() {
+  return <ListContainer fetchFunction={fetchItemsFilms} />;
+}
 
 export default function Films({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden={false} barStyle="dark-content" />
       <Text style={styles.title}>Welcome to the Films Page</Text>
       <View style={styles.box}>
-        <Text style={styles.boxText}>Discover the Star Wars Films here!</Text>
+        <Text style={styles.boxText}>DISCOVER THE STARWARS FILMS HERE!</Text>
       </View>
-    </View>
+      <View style={styles.container}>
+        <ListContainerFilms />
+      </View>
+    </SafeAreaView>
   );
 }
