5f03cd88a056c6eb661870bcea5a3791c2756402
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-11-15 01:22:14 -0500

    Add files via upload

---
+++ components/ListFilter.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"; // Added `useState` import
+import PropTypes from "prop-types";
+import { View, TextInput } from "react-native";
+import styles from "./styles";
+import PassiveNotifications from "../notifications/PassiveNotifications";
+import Notification from "../notifications/Notification.ios";
+
+export default function ListFilter({ onFilter }) {
+  const [changedText, setChangedText] = useState("");
+
+  return (
+    <View>
+      <TextInput
+        autoFocus
+        placeholder="Search"
+        style={styles.filter}
+        
+        onSubmitEditing={(e) => {
+          setChangedText(e.nativeEvent.text);
+        }}
+        onFocus={() => {
+          setChangedText("");
+          
+        }}
+      />
+      {/* Trigger the Notification component with the current text */}
+      {changedText && <Notification message={changedText} />}
+    </View>
+  );
+}
+
+ListFilter.propTypes = {
+  onFilter: PropTypes.func.isRequired,
+};
