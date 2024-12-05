9441aa553f5e34c34cfba711f2bd7705dbd6cd11
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-12-05 16:26:48 -0500

    Update List.js

--- components/List.js
+++ components/List.js
@@ -1,27 +1,42 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Text, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { FlatList, View } from "react-native";
 import styles from "./styles";
-import ListControls from "./ListControls";
+import Notification from "../notifications/Notification";
+import Swipeable from "../Swipeable";
 
-export default function List({ Controls, data, onFilter, onSort, asc }) {
+export default function List({ data }) {
+  const [datas, setData] = useState(data);
+  const [notificationMessage, setNotificationMessage] = useState(null);
+
+  useEffect(() => {
+    setData(data); // Sync internal state when prop changes
+  }, [data]);
+
+  const handleSwipe = (id) => {
+    const item = datas.find((item) => item.id === id);
+    if (item) {
+      setNotificationMessage(`You swiped: ${item.name}`);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <Swipeable name={item.name} onSwipe={() => handleSwipe(item.id)} />
+  );
+
   return (
-    <FlatList
-      data={data}
-      ListHeaderComponent={<Controls {...{ onFilter, onSort, asc }} />}
-      renderItem={({ item }) => <Text style={styles.item}>{item.value}</Text>}
-    />
+    <View style={styles.container}>
+      <FlatList
+        data={datas}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+      {notificationMessage && (
+        <Notification
+          message={notificationMessage}
+          duration={3000}
+          onDismiss={() => setNotificationMessage(null)} 
+        />
+      )}
+    </View>
   );
 }
-
-List.propTypes = {
-  Controls: PropTypes.func.isRequired,
-  data: PropTypes.array.isRequired,
-  onFilter: PropTypes.func.isRequired,
-  onSort: PropTypes.func.isRequired,
-  asc: PropTypes.bool.isRequired,
-};
-
-List.defaultProps = {
-  Controls: ListControls,
-};
