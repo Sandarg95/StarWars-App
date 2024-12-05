50b489705d6f28377c3c2286f38da9415544bbc0
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-12-05 16:29:55 -0500

    Create Swipeable.js

---
+++ Swipeable.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, ScrollView, Text, TouchableOpacity,Platform } from "react-native";
+import styles from "./components/styles";
+
+export default function Swipeable({ onSwipe, name }) {
+
+  function onScroll(e) {
+    const offsetX = e.nativeEvent.contentOffset.x;
+    const threshold = Platform.OS === "ios" ? 5 : 6; 
+    if (offsetX >= threshold - 1 && offsetX <= threshold + 1) {
+      onSwipe();
+    }
+  }
+
+
+  const scrollProps = {
+    horizontal: true,
+    pagingEnabled: true,
+    showsHorizontalScrollIndicator: false,
+    scrollEventThrottle: 10,
+    onScroll,
+  };
+
+  return (
+    <View style={styles.swipeContainer}>
+      <ScrollView {...scrollProps}>
+        <TouchableOpacity>
+          <View style={styles.swipeItem}>
+            <Text style={styles.swipeItemText}>{name}</Text>
+          </View>
+        </TouchableOpacity>
+        <View style={styles.swipeBlank} />
+      </ScrollView>
+    </View>
+  );
+}
