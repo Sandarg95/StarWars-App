a92f0cb9703cc146716457a22957d0ac9da622a1
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-12-05 21:51:14 -0500

    Update SpaceScreen.js

--- screen/SpaceScreen.js
+++ screen/SpaceScreen.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StatusBar, SafeAreaView } from "react-native";
 import styles from "../components/styles";
 import ListContainer from "../components/ListContainer";
-import { fetchItemsSpace } from "../api"; // Import the fetch function
+import { fetchItemsFilms } from "../api"; 
+import LazyImage from "../LazyImage";
 
-function ListContainerSpace() {
-  return <ListContainer fetchFunction={fetchItemsSpace} />;
+function ListContainerFilms() {
+  return <ListContainer fetchFunction={fetchItemsFilms} />;
 }
 
-export default function Space({ navigation }) {
+const remote = "https://lumiere-a.akamaihd.net/v1/images/vaders-tie-fighter_8bcb92e1.jpeg";
+
+export default function Films({ navigation }) {
+  const [source, setSource] = useState(null);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar hidden={false} barStyle="dark-content" />
       <Text style={styles.title}>Welcome to the Space Page</Text>
+      <LazyImage
+        style={styles.images}
+        resizeMode="contain"
+        source={{ uri: remote }}  
+      />
       <View style={styles.box}>
         <Text style={styles.boxText}>DISCOVER THE STARWARS SPACESHIP HERE!</Text>
       </View>
       <View style={styles.container}>
-        <ListContainerSpace />
+        <ListContainerFilms />
       </View>
     </SafeAreaView>
   );
