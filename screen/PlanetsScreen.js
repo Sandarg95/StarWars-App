4dc5d4c4c3e63835b76a45f488d3c6b11ddc1ea2
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-12-05 21:51:03 -0500

    Update PlanetsScreen.js

--- screen/PlanetsScreen.js
+++ screen/PlanetsScreen.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StatusBar, SafeAreaView } from "react-native";
 import styles from "../components/styles";
 import ListContainer from "../components/ListContainer";
-import { fetchItemsPlanets } from "../api"; 
+import { fetchItemsFilms } from "../api"; 
+import LazyImage from "../LazyImage";
 
-function ListContainerPlanets() {
-  return <ListContainer fetchFunction={fetchItemsPlanets} />;
+function ListContainerFilms() {
+  return <ListContainer fetchFunction={fetchItemsFilms} />;
 }
 
-export default function Planets({ navigation }) {
+const remote = "https://lumiere-a.akamaihd.net/v1/images/aeos-prime-main_1af6e847.jpeg";
+
+export default function Films({ navigation }) {
+  const [source, setSource] = useState(null);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar hidden={false} barStyle="dark-content" />
       <Text style={styles.title}>Welcome to the Planets Page</Text>
+      <LazyImage
+        style={styles.images}
+        resizeMode="contain"
+        source={{ uri: remote }}  
+      />
       <View style={styles.box}>
         <Text style={styles.boxText}>DISCOVER THE STARWARS PLANETS HERE!</Text>
       </View>
       <View style={styles.container}>
-        <ListContainerPlanets />
+        <ListContainerFilms />
       </View>
     </SafeAreaView>
   );
