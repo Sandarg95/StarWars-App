25134147af8de5362ecadf6f239159bd66a1f41e
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-12-15 21:04:58 -0500

    Update List.js

--- components/List.js
+++ components/List.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, View } from "react-native";
+import { FlatList, View, Text } from "react-native";
+import PropTypes from "prop-types";
 import styles from "./styles";
 import Notification from "../notifications/Notification";
 import Swipeable from "../Swipeable";
+import ListControls from "./ListControls"; // Assuming ListControls is in the same directory
 
-export default function List({ data }) {
-  const [datas, setData] = useState(data);
+export default function List({ data, Controls, onFilter, onSort, asc }) {
   const [notificationMessage, setNotificationMessage] = useState(null);
 
-  useEffect(() => {
-    setData(data); // Sync internal state when prop changes
-  }, [data]);
-
   const handleSwipe = (id) => {
-    const item = datas.find((item) => item.id === id);
+    const item = data.find((item) => item.id === id);
     if (item) {
       setNotificationMessage(`You swiped: ${item.name}`);
     }
@@ -24,19 +21,35 @@ export default function List({ data }) {
   );
 
   return (
-    <View style={styles.container}>
+    <View>
       <FlatList
-        data={datas}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
+        data={data}
+        keyExtractor={(item) => item.id.toString()} 
+        ListHeaderComponent={
+          <Controls onFilter={onFilter} onSort={onSort} asc={asc} />
+        }
+        renderItem={renderItem} 
       />
       {notificationMessage && (
-        <Notification
-          message={notificationMessage}
-          duration={3000}
-          onDismiss={() => setNotificationMessage(null)} 
-        />
+        <Notification message={notificationMessage} /> 
       )}
     </View>
   );
 }
+
+List.propTypes = {
+  Controls: PropTypes.func.isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onFilter: PropTypes.func.isRequired,
+  onSort: PropTypes.func.isRequired,
+  asc: PropTypes.bool.isRequired,
+};
+
+List.defaultProps = {
+  Controls: ListControls,
+};
